cf5e6be59901eaec672a7f761b6f0f28db0ea0ed
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-17 16:18:01 +0300

    fix date send mail

--- src/main/java/com/example/hospital/controller/RegistrationController.java
+++ src/main/java/com/example/hospital/controller/RegistrationController.java
@@ -2,41 +2,65 @@ package com.example.hospital.controller;
 
 import com.example.hospital.Service.EmailService;
 import com.example.hospital.model.Client;
+import com.example.hospital.model.Doctor;
 import com.example.hospital.repository.ClientRepository;
+import com.example.hospital.repository.DoctorRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 @AllArgsConstructor
-
-
 public class RegistrationController {
-   private final ClientRepository clientRepository;
-   private final EmailService emailService;
+
+    private final ClientRepository clientRepository;
+    private final EmailService emailService;
+    private final DoctorRepository doctorRepository;
 
     @PostMapping("/register")
-    public String registerUser(@RequestParam String name, @RequestParam String email, @RequestParam String phone) {
+    public String registerUser(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String phone,
+            @RequestParam String date,
+            @RequestParam Long doctorId) {
+
         // Создание нового объекта клиента
         Client client = new Client();
         client.setName(name);
         client.setEmail(email);
         client.setPhone(phone);
 
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+        try {
+            Date appointmentDate = formatter.parse(date);
+            client.setAppointmentDate(appointmentDate);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return "Ошибка при обработке даты и времени";
+        }
+
+        // Получаем выбранного врача из базы данных
+        Doctor doctor = doctorRepository.findById(doctorId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid doctor Id: " + doctorId));
+
+        // Привязываем клиента к выбранному врачу
+        client.setDoctor(doctor);
+
+        // Сохраняем клиента в базе данных
+        clientRepository.save(client);
+
         // Отправка письма подтверждения на электронную почту
         String subject = "Запись на прием";
         String message = "Вы успешно записались на прием!";
         emailService.sendSimpleMessage(email, subject, message);
 
-        // Предположим, что здесь будет код для сохранения клиента в базу данных
-        clientRepository.save(client);
-
-
         return "Запись на прием успешно создана. Письмо отправлено на " + email;
     }
-
 }
